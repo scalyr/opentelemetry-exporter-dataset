4b2e6a8bfe9782a168fde483c781c28369d08985
Tomaz Muraus <tomazm@sentinelone.com>
2023-08-25 10:00:04 +0200

    Squashed 'scalyr-opentelemetry-collector-contrib/' content from commit 26b3cceb24
    git-subtree-dir: scalyr-opentelemetry-collector-contrib
    git-subtree-split: 26b3cceb247672d3c443481384cee9f663e37bfa

---
+++ receiver/redisreceiver/config.go
@@ -0,0 +1,30 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package redisreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/redisreceiver"
+
+import (
+	"go.opentelemetry.io/collector/config/confignet"
+	"go.opentelemetry.io/collector/config/configopaque"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/receiver/scraperhelper"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/redisreceiver/internal/metadata"
+)
+
+type Config struct {
+	scraperhelper.ScraperControllerSettings `mapstructure:",squash"`
+	// TODO: Use one of the configs from core.
+	// The target endpoint.
+	confignet.NetAddr `mapstructure:",squash"`
+
+	// TODO allow users to add additional resource key value pairs?
+
+	// Optional password. Must match the password specified in the
+	// requirepass server configuration option.
+	Password configopaque.String `mapstructure:"password"`
+
+	TLS configtls.TLSClientSetting `mapstructure:"tls,omitempty"`
+
+	MetricsBuilderConfig metadata.MetricsBuilderConfig `mapstructure:",squash"`
+}
